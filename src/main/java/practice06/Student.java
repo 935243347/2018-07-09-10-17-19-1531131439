5ffe60671951ed004a26fdee77bbfaca9c641c8f
Max Miao <miaoma2@oocl.com>
2020-07-22 17:12:00 +0800

    oo[Max]feature: practice06

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,19 @@
 package practice06;
 
-public class Student {
+import practice04.Person;
+
+public class Student extends Person {
+    protected int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public String introduce(){
+        return super.introduce() + " I am a Student. I am at Class " + this.klass + ".";
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
