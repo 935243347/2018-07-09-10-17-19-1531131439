73fff7334d986fef6a295a039955bbbf303196a3
Max Miao <miaoma2@oocl.com>
2020-07-22 20:21:37 +0800

    oo[Max]feature: practice10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,38 @@
 package practice10;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass() {
+        this.number = 0;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName(){
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student student){
+        if(student.getKlass().number == this.number){
+            this.leader = student;
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student){
+        student.setKlass(this);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,53 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+    private LinkedList<Klass> classes;
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        this.classes = null;
+    }
+    public String introduce(){
+        if(this.classes == null){
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        } else {
+            String tmpString = super.introduce() + " I am a Teacher. I teach Class ";
+            for(int i = 0; i < classes.size(); i++){
+                if(i == 0){
+                    tmpString += classes.get(i).getNumber();
+                } else {
+                    tmpString = tmpString + ", " + classes.get(i).getNumber();
+                }
+            }
+            tmpString += ".";
+            return  tmpString;
+        }
+
+    }
+    public String introduceWith(Student student){
+        for(int i = 0; i < classes.size(); i++){
+            if(student.getKlass().getNumber() == this.classes.get(i).getNumber()){
+                return super.introduce() + " I am a Teacher. I teach " + student.name+ ".";
+            }
+        }
+        return super.introduce() + " I am a Teacher. I don't teach " + student.name+ ".";
+    }
+    public boolean isTeaching(Student student){
+        for(int i = 0; i < classes.size(); i++){
+            if(student.getKlass().getNumber() == this.classes.get(i).getNumber()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
 }
